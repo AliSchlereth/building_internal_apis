350665abd04923bb80afd94f728e68b84194f361
Ali Schlereth <aschlere@sbcglobal.net>
2017-03-14 09:46:34 -0600

    Add column price to items

---
+++ db/migrate/20170314153931_add_price_to_integer.rb
@@ -0,0 +1,5 @@
+class AddPriceToInteger < ActiveRecord::Migration[5.0]
+  def change
+    add_column :items, :price, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314031639) do
+ActiveRecord::Schema.define(version: 20170314153931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170314031639) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "price"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "order_number"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
